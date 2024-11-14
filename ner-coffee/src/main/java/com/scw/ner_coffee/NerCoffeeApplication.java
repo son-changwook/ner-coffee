912063b61dbbe1d170cc5b88e2bde7412ff88525
sonchangwook <changw8k20@gmail.com>
2024-11-14 14:37:36 +0900

    start

---
+++ ner-coffee/src/main/java/com/scw/ner_coffee/NerCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.scw.ner_coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerCoffeeApplication.class, args);
+	}
+
+}
---
+++ ner-coffee/src/test/java/com/scw/ner_coffee/NerCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scw.ner_coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
